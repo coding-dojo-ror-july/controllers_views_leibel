1d9ea51c48307f929b793258bec0136fc6982cb6
Leibelhecht <leibelhecht@gmail.com>
2017-07-12 14:11:25 -0500

    dojo_dashboardII

--- dojo_dashboard_I/config/routes.rb
+++ dojo_dashboard_I/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  
-  get 'dojos/index' => "dojos#index"
-  get "/" => "dojos#index"
+  root 'dojos#index'  
+  get 'dojos' => "dojos#index"
+  get 'dojos/new' => "dojos#new"  
+  post 'dojos' => "dojos#create"  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
